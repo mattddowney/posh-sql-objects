11254f3dabe32abfdc396891d8c87e6c837f04ca
Matt Downey <mattddowney@gmail.com>
2016-11-27 14:51:13 -0700

    change path to %PSModulePath%

--- install.cmd
+++ install.cmd
@@ -1,3 +1,3 @@
-SET module_folder=%HOMEDRIVE%\%HOMEPATH%\Documents\WindowsPowerShell\Modules\posh-sql-objects
+SET module_folder=%PSModulePath%\posh-sql-objects
 mkdir %module_folder%
 robocopy posh-sql-objects %module_folder% /MIR /IS
