50ab6e9162dbee99a4b663b4a627cf04474edd0d
Matt Downey <mattddowney@gmail.com>
2016-12-28 12:04:46 -0700

    better install script

--- install.cmd
+++
@@ -1,3 +0,0 @@
-SET module_folder=%PSModulePath%\posh-sql-objects
-mkdir %module_folder%
-robocopy posh-sql-objects %module_folder% /MIR /IS
---
+++ install.ps1
@@ -0,0 +1,4 @@
+﻿$installDir = ($env:PSModulePath).Split(';')[0]
+Write-Host "Installing to $installDir"
+Copy-Item posh-sql-objects $installDir -Recurse
+Write-Host "Done"
